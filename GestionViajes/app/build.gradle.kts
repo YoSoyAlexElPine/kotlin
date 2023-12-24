72838631dfbfc3ac73ba3cccc00e6845f3627818
Alex Pineño Sanchez <119886032+YoSoyAlexElPine@users.noreply.github.com>
2023-12-24 17:44:34 +0100

    Sd
    ret

--- GestionViajes/app/build.gradle.kts
+++ GestionViajes/app/build.gradle.kts
@@ -48,9 +48,9 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
     implementation ("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("com.google.firebase:firebase-firestore-ktx:24.9.1")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.10.0")
     implementation("com.google.firebase:firebase-analytics:21.5.0")
-    implementation("com.google.firebase:firebase-auth:22.2.0")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("com.google.firebase:firebase-messaging-ktx:23.4.0")
 
     testImplementation("junit:junit:4.13.2")
@@ -58,5 +58,5 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
 
-    implementation("com.google.android.material:material:1.10.0") // Asegúrate de tener las dependencias adecuadas.
+    implementation("com.google.android.material:material:1.11.0") // Asegúrate de tener las dependencias adecuadas.
 }
